ce2f14fb734c80b4360462f3e734ff046e367ec2
Live session user <lucaspdfborges@gmail.com>
2019-10-09 12:06:09 +0000

    fixed profile img

--- website/accounts/forms.py
+++ website/accounts/forms.py
@@ -13,7 +13,7 @@ class SignUpForm(UserCreationForm):
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'username', 'email')
 
 class ProfileForm(forms.ModelForm):
     class Meta:
